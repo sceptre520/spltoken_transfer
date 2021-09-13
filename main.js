4a64826a73ed3399b4b27490da9d384f53faec73
sceptre520 <denysshowdev@gmail.com>
2021-09-13 10:17:12 +0300

    tgt3

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ const cron = require('node-cron');
 const web3 =  require("@solana/web3.js");
 const splToken =  require("@solana/spl-token");
 
-cron.schedule("0 */1 * * * *", function() {
-    transfer()
-})
 
 const MASTER_WALLET_SECRET_KEY = new Uint8Array([53,148,141,100,246,232,104,140,58,13,31,238,37,162,102,57,7,167,189,96,107,141,179,175,243,162,109,175,223,15,60,227,102,252,240,2,32,225,103,200,75,194,77,217,120,204,30,182,50,237,141,3,193,151,150,200,203,5,81,154,11,191,228,151]);
 const NETWORK = "devnet"
@@ -17,11 +14,11 @@ const TGT2_PUBKEY = "AAsxTQ8q9KQfuvRDbD9tP5MrMzdAsC4n8x3n3Dc84UTt"
 const TGT2_AMOUNT = 100000
 const TGT3_PUBKEY = "CTtdDqVntRM1igfGTK9eXMBDFM9ho4XQpjLSsrusWQRc"
 const TGT3_AMOUNT = 30000
-const transfer = async () => {
+const transfer = async (tgt_pub, tgt_amount) => {
   console.log('--------------------------------------------------------------')
   var connection = new web3.Connection(web3.clusterApiUrl(NETWORK));
   var fromWallet = web3.Keypair.fromSecretKey(MASTER_WALLET_SECRET_KEY);
-  var toWalletPub = new web3.PublicKey(TGT1_PUBKEY);
+  var toWalletPub = new web3.PublicKey(tgt_pub);
   var myMint = new web3.PublicKey(TOKEN_PUB_KEY);
   var myToken = new splToken.Token(
     connection,
@@ -45,7 +42,7 @@ const transfer = async () => {
         toTokenAccount.address,
         fromWallet.publicKey,
         [],
-        TGT1_AMOUNT
+        tgt_amount
       )
     );
   
@@ -57,3 +54,9 @@ const transfer = async () => {
   console.log("SIGNATURE", signature);
   console.log("SUCCESS");
 }
+
+cron.schedule("0 */1 * * * *", function() {
+  transfer(TGT1_PUBKEY, TGT1_AMOUNT)
+  transfer(TGT2_PUBKEY, TGT2_AMOUNT)
+  transfer(TGT3_PUBKEY, TGT3_AMOUNT)
+})
